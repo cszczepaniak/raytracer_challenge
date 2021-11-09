43421aee544a70e236ba2d81374a7d886bf07269
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-11-08 23:02:37 -0600

    implement PPM image format

--- src/tuples/color.rs
+++ src/tuples/color.rs
@@ -15,6 +15,15 @@ impl Color {
     pub fn new(r: f64, g: f64, b: f64) -> Self {
         Tuple(r, g, b, 0.0, PhantomData)
     }
+
+    pub fn to_bytes(&self) -> (u8, u8, u8) {
+        let scaled = *self * 255.0;
+        (
+            scaled.0.clamp(0.0, 255.0).round() as u8,
+            scaled.1.clamp(0.0, 255.0).round() as u8,
+            scaled.2.clamp(0.0, 255.0).round() as u8,
+        )
+    }
 }
 
 impl PartialEq for Color {
