ff014dde9cfd744378be72e9d8a2f13f9f0ce3c2
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-12-02 20:23:10 -0600

    camera stuff

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,4 @@
-use crate::{matrix::Matrix, point::Point, ray::Ray};
+use crate::{matrix::Matrix, point::Point, ray::Ray, vector::Vector};
 
 pub struct Camera {
     pub transform: Matrix<4>,
@@ -37,6 +37,27 @@ impl Camera {
 
     pub fn with_transform(self, transform: Matrix<4>) -> Self {
         Self { transform, ..self }
+    }
+
+    pub fn look_at_from_position(self, from: Point, to: Point, up: Vector) -> Self {
+        let forward = (to - from).normalize();
+        let left = forward.cross(&up.normalize());
+        let true_up = left.cross(&forward);
+
+        #[rustfmt::skip]
+        let orientation = Matrix::from([
+            [left[0],     left[1],     left[2],     0.0],
+            [true_up[0],  true_up[1],  true_up[2],  0.0],
+            [-forward[0], -forward[1], -forward[2], 0.0],
+            [0.0,         0.0,         0.0,         1.0],
+        ]);
+
+        let translation = Matrix::translate(-from[0], -from[1], -from[2]);
+
+        Self {
+            transform: orientation * translation,
+            ..self
+        }
     }
 
     pub fn ray_for_pixel(&self, x: usize, y: usize) -> Ray {
@@ -110,5 +131,53 @@ mod tests {
 
         assert_fuzzy_eq!(Point::new(0.0, 2.0, -5.0), r.origin);
         assert_fuzzy_eq!(Vector::new(FRAC_1_SQRT_2, 0.0, -FRAC_1_SQRT_2), r.direction);
+    }
+
+    #[test]
+    fn view_transform_for_the_default_orientation() {
+        let from = Point::new(0.0, 0.0, 0.0);
+        let to = Point::new(0.0, 0.0, -1.0);
+        let up = Vector::new(0.0, 1.0, 0.0);
+        let camera = Camera::new(100, 100, FRAC_PI_2).look_at_from_position(from, to, up);
+
+        assert_fuzzy_eq!(Matrix::<4>::identity(), camera.transform);
+    }
+
+    #[test]
+    fn view_transform_looking_into_positive_z_direction() {
+        let from = Point::new(0.0, 0.0, 0.0);
+        let to = Point::new(0.0, 0.0, 1.0);
+        let up = Vector::new(0.0, 1.0, 0.0);
+        let camera = Camera::new(100, 100, FRAC_PI_2).look_at_from_position(from, to, up);
+
+        assert_fuzzy_eq!(Matrix::scale(-1.0, 1.0, -1.0), camera.transform);
+    }
+
+    #[test]
+    fn view_transform_moves_the_world() {
+        let from = Point::new(0.0, 0.0, 8.0);
+        let to = Point::new(0.0, 0.0, 0.0);
+        let up = Vector::new(0.0, 1.0, 0.0);
+        let camera = Camera::new(100, 100, FRAC_PI_2).look_at_from_position(from, to, up);
+
+        assert_fuzzy_eq!(Matrix::translate(0.0, 0.0, -8.0), camera.transform);
+    }
+
+    #[test]
+    fn arbitrary_view_transformation() {
+        let from = Point::new(1.0, 3.0, 2.0);
+        let to = Point::new(4.0, -2.0, 8.0);
+        let up = Vector::new(1.0, 1.0, 0.0);
+        let camera = Camera::new(100, 100, FRAC_PI_2).look_at_from_position(from, to, up);
+
+        assert_fuzzy_eq!(
+            Matrix::from([
+                [-0.50709, 0.50709, 0.67612, -2.36643],
+                [0.76772, 0.60609, 0.12122, -2.82843],
+                [-0.35857, 0.59761, -0.71714, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ]),
+            camera.transform
+        )
     }
 }
